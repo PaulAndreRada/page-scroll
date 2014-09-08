af90ea2fdff3d4e4c73dda412309590127d7b5ed
Paul Andre Rada <paulriverarada@gmail.com>
2014-09-08 15:36:34 -0700

    replaced the scroll event with a mousewheel event that supports firefox in record time and sang kumbaya

--- js/vertical-carousel-ck.js
+++ js/vertical-carousel-ck.js
@@ -1,1 +1,67 @@
-$(function(){var e=function(e){var t={},n=$(document),r=n.find(e),i=r.find("ul"),s=i.find("li"),o=0,u=s.length,a=0,f=450,l;t.init=function(){t.setPaneDimensions();$(window).on("load resize orientationchange",function(){t.setPaneDimensions()});return t};t.setPaneDimensions=function(){o=r.height();for(var e=0;e<u;e++)s.height(o);i.height(o*u);return t};t.showPane=function(e){var e=Math.max(0,Math.min(e,u-1));a=e;var n=-(100/u*a);t.setContainerOffset(n,!0);return t};t.setContainerOffset=function(e,t){i.removeClass("animate");t&&i.addClass("animate");if(Modernizr.csstransforms3d)i.css("transform","translate3d(0,"+e+"%,0) scale3d(1,1,1)");else if(Modernizr.csstransforms)i.css("transform","translate(0,"+e+"%)");else{var n=o*u/100*e;i.css("top",n+"px")}};t.next=function(){return t.showPane(a+1)};t.prev=function(){return t.showPane(a-1)};var c=function(e){e.gesture.preventDefault();switch(e.type){case"dragup":case"dragdown":var n=-(100/u)*a,r=100/o*e.gesture.deltaY/u;if(a==0&&e.gesture.direction==Hammer.DIRECTION_DOWN||a==u-1&&e.gesture.direction==Hammer.DIRECTION_UP)r*=.4;t.setContainerOffset(r+n);break;case"swipeup":t.next();e.gesture.stopDetect();break;case"swipedown":t.prev();e.gesture.stopDetect();break;case"release":Math.abs(e.gesture.deltaY)>o/2?e.gesture.direction==="down"?t.prev():t.next():t.showPane(a,!0)}},h=function(e){var n=$(this),r=(new Date).getTime();if(l+f>=r){e.preventDefault();e.stopPropagation();return}if(n.scrollTop()===1){e.preventDefault();e.stopPropagation();t.next();l=r}else if(n.scrollTop()===-1){e.preventDefault();e.stopPropagation();t.prev();l=r}};$(window).scroll(h);r.hammer({drag_to_lock_target:!0}).on("release dragdown dragup swipeleft swiperight swipeup swipedown",c);return t},t=e("#carousel");t.init()});
+/*------------------------------------------------*//*  Credit: Eike Send for the swipe event *//*------------------------------------------------*//*
+// @swipe event
+(function($) { 
+  $.fn.swipeEvents = function() {
+    return this.each(function() {
+      
+      var startX,
+          startY,
+          $this = $(this); // no need, its doubling up
+      
+      $this.bind('touchstart', touchstart);
+      
+      function touchstart(event) {
+        //
+        //grab the touches event info
+        var touches = event.originalEvent.touches;
+
+
+        if (touches && touches.length) {
+          //
+          // grab the matrix information for those events
+          startX = touches[0].pageX;
+          startY = touches[0].pageY;
+          //
+          // if touches exist bind touchmove and touch end
+          $this.bind('touchmove', touchmove);
+          $this.bind('touchend', touchend);
+        }
+        event.preventDefault();
+      }
+      
+      function touchmove(event) {
+        var touches = event.originalEvent.touches;
+        if (touches && touches.length) {
+          var deltaX = startX - touches[0].pageX;
+          var deltaY = startY - touches[0].pageY;
+          
+          if (deltaX >= 50) {
+            $this.trigger("swipeLeft");
+          }
+          if (deltaX <= -50) {
+            $this.trigger("swipeRight");
+          }
+          if (deltaY >= 50) {
+            $this.trigger("swipeUp");
+          }
+          if (deltaY <= -50) {
+            $this.trigger("swipeDown");
+          }
+          if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
+            $this.unbind('touchmove', touchmove);
+            $this.unbind('touchend', touchend);
+          }
+        }
+        event.preventDefault();
+      }
+      
+      function touchend(event) {
+        $this.unbind('touchmove', touchmove);
+        event.preventDefault();
+      }
+      
+    });
+  };
+})(jQuery);
+*///@carousel
+$(function(){var e=function(e){var t={},n=$(document),r=n.find(e),i=r.find("ul"),s=i.find("li"),o=0,u=s.length,a=0,f=450,l=1e3,c=0;t.init=function(){t.setPaneDimensions();$(window).on("load resize orientationchange",function(){t.setPaneDimensions()});return t};t.setPaneDimensions=function(){o=r.height();for(var e=0;e<u;e++)s.height(o);i.height(o*u);return t};t.showPane=function(e){var e=Math.max(0,Math.min(e,u-1));a=e;var n=-(100/u*a);t.setContainerOffset(n,!0);return t};t.setContainerOffset=function(e,t){i.removeClass("animate");t&&i.addClass("animate");if(Modernizr.csstransforms3d)i.css("transform","translate3d(0,"+e+"%,0) scale3d(1,1,1)");else if(Modernizr.csstransforms)i.css("transform","translate(0,"+e+"%)");else{var n=o*u/100*e;i.css("top",n+"px")}};t.next=function(){return t.showPane(a+1)};t.prev=function(){return t.showPane(a-1)};var h=function(e,n){var r=$(this),i=n,s=(new Date).getTime(),o=s-c,u=f+l,a=r.scrollTop();if(o<u){e.preventDefault();return}i<0?t.next():t.prev();c=s};n.bind("mousewheel DOMMouseScroll MozMousePixelScroll",function(e){e.preventDefault();var t=e.originalEvent.wheelDelta||-e.originalEvent.detail;h(e,t)});var p=function(e){e.gesture.preventDefault();switch(e.type){case"dragup":case"dragdown":var n=-(100/u)*a,r=100/o*e.gesture.deltaY/u;if(a==0&&e.gesture.direction==Hammer.DIRECTION_DOWN||a==u-1&&e.gesture.direction==Hammer.DIRECTION_UP)r*=.4;t.setContainerOffset(r+n);break;case"swipeup":t.next();e.gesture.stopDetect();break;case"swipedown":t.prev();e.gesture.stopDetect();break;case"release":Math.abs(e.gesture.deltaY)>o/2?e.gesture.direction==="down"?t.prev():t.next():t.showPane(a,!0)}};r.hammer({drag_to_lock_target:!0}).on("release dragdown dragup swipeleft swiperight swipeup swipedown",p);return t},t=e("#carousel");t.init()});
--- js/vertical-carousel.js
+++ js/vertical-carousel.js
@@ -1,5 +1,80 @@
+
+
+
+/*------------------------------------------------*/
+/*  Credit: Eike Send for the swipe event */
+/*------------------------------------------------*/
+/*
+// @swipe event
+(function($) { 
+  $.fn.swipeEvents = function() {
+    return this.each(function() {
+      
+      var startX,
+          startY,
+          $this = $(this); // no need, its doubling up
+      
+      $this.bind('touchstart', touchstart);
+      
+      function touchstart(event) {
+        //
+        //grab the touches event info
+        var touches = event.originalEvent.touches;
+
+
+        if (touches && touches.length) {
+          //
+          // grab the matrix information for those events
+          startX = touches[0].pageX;
+          startY = touches[0].pageY;
+          //
+          // if touches exist bind touchmove and touch end
+          $this.bind('touchmove', touchmove);
+          $this.bind('touchend', touchend);
+        }
+        event.preventDefault();
+      }
+      
+      function touchmove(event) {
+        var touches = event.originalEvent.touches;
+        if (touches && touches.length) {
+          var deltaX = startX - touches[0].pageX;
+          var deltaY = startY - touches[0].pageY;
+          
+          if (deltaX >= 50) {
+            $this.trigger("swipeLeft");
+          }
+          if (deltaX <= -50) {
+            $this.trigger("swipeRight");
+          }
+          if (deltaY >= 50) {
+            $this.trigger("swipeUp");
+          }
+          if (deltaY <= -50) {
+            $this.trigger("swipeDown");
+          }
+          if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
+            $this.unbind('touchmove', touchmove);
+            $this.unbind('touchend', touchend);
+          }
+        }
+        event.preventDefault();
+      }
+      
+      function touchend(event) {
+        $this.unbind('touchmove', touchmove);
+        event.preventDefault();
+      }
+      
+    });
+  };
+})(jQuery);
+*/
+
+
+
+//@carousel
 $(function(){ 
-	
 	//	
 	var VerticalCarousel = function( element ){ 
 	        
@@ -12,7 +87,8 @@ $(function(){
 	    paneCount = $panes.length,
 	    currentPane = 0,
 	    animationTime = 450,
-	    lastAnimationTime;
+	    quietPeriod = 1000,
+	    lastAnimationTime = 0;
 	    
 
 	    VC.init = function(){ 
@@ -35,6 +111,7 @@ $(function(){
 		for ( var i=0; i<paneCount; i++ ){ 
 		    // 
 		    $panes.height( paneHeight );
+		    //
 		};
 		//
 		$container.height( paneHeight * paneCount );
@@ -57,7 +134,9 @@ $(function(){
 		//
 		VC.setContainerOffset( offset, true );
 		//
+		//
 		return VC;
+		//
 	    };
 	        
 
@@ -86,25 +165,74 @@ $(function(){
 
 	    VC.next = function(){ 
 		// 
+		//
 		return VC.showPane( currentPane + 1 );
 		//
 	    };
 
 	    VC.prev = function(){ 
 		//
+		//
 		return VC.showPane( currentPane - 1);
 		//
 	    };
 	    
+	    
+	    var handleScroll = function( event, delta ){ 
+		//
+		var $this = $(this),
+		deltaOfInterest = delta,
+		timeNow = new Date().getTime(),
+		timeElapsed = timeNow - lastAnimationTime,
+		waitTime = animationTime + quietPeriod,
+		scrollTop = $this.scrollTop();
+		//
+		//
+		// Cancel scroll if currently animating or within quiet period
+		if( timeElapsed < waitTime ){
+		    //
+		    event.preventDefault();
+		    //
+		    // leave the function
+		    return;
+		}
+		//
+		// if delta is negative call prev()
+		if (deltaOfInterest < 0) {
+		    VC.next()
+		} else {
+		    VC.prev()
+		}
+		// set the last animations time
+		lastAnimationTime = timeNow;
+		//
+	    };
+	    //	    
+	    // @DESKTOP HANDLER
+	    // activate the carousel event handler for the desktop
+	    // make event handler
+	    $doc.bind('mousewheel DOMMouseScroll MozMousePixelScroll', function(event) {
+		    //
+		    event.preventDefault();
+		    // 
+		    // assing the delta depending on the brower [ webkit and presto || mozilla ]
+		    var delta = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+		    //
+		    // call scroll function
+		    handleScroll(event, delta);
+		});
+	    
+
+	    	    
 	    var handleTouch = function( ev ){ 
 		//
 		// disable browser scrolling
 		ev.gesture.preventDefault();
-
+		//
 		switch(ev.type) {
                 case 'dragup':
                 case 'dragdown':
-
+		    //
                     // stick to the finger
                     var pane_offset = -(100/paneCount)*currentPane;
                     var drag_offset = ((100/paneHeight)*ev.gesture.deltaY)/ paneCount;
@@ -152,58 +280,22 @@ $(function(){
                     break;
 		}
 	    };
-	    
-	    var handleScroll = function( e ){ 
-		//
-		var $this = $(this),
-		timeNow = new Date().getTime();
-		
-		
-		if ( lastAnimationTime + animationTime >= timeNow ){ 
-		    //
-		    // leave function as the slide is still adjusting
-		    e.preventDefault();
-		    e.stopPropagation();
-		    return;
-		    //
-		} else { 
-		    //
-		    if ( $this.scrollTop() === 1 ){
-			e.preventDefault();
-			e.stopPropagation();
-			//
-			VC.next();
-			//
-			lastAnimationTime = timeNow;
-			//
-		    } else if ( $this.scrollTop() === -1){ 
-			e.preventDefault();
-			e.stopPropagation();
-			//
-			VC.prev();
-			//
-			lastAnimationTime = timeNow;
-			//
-		    };
-		    
-		}
-	    };
-	    
-	    // activate the carousel event handler for the desktop
-	    $(window).scroll( handleScroll );
 	    //
+	    // @MOBILE HANDLER
 	    // activate the carousel event handler for mobile
 	    $element.hammer({ drag_to_lock_target : true })
-	    .on( 'release dragdown dragup swipeleft swiperight swipeup swipedown', handleTouch );
-	    //
+	    .on( 'release dragdown dragup swipeleft swiperight swipeup swipedown',
+		 handleTouch );
+	    
+	    
+	    
+	    
+	    
 	    return VC;
 	    //
 	};//verticalCarousel
 	
+	var Carousel = VerticalCarousel( '#carousel' );
+	Carousel.init();
 	
-	
-	var vertTest = VerticalCarousel( '#carousel' );
-	vertTest.init();
-	
-	
-    });
+    });// end 
